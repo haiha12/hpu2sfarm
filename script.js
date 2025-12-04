6016cdae4a378c2148c941bef1506e7ec5b82655
haiha12 <xuanha221@gmail.com>
2025-12-04 22:12:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -221,7 +221,6 @@ function switchTab(tabId, el) {
             currentAudio = new Audio(soundUrl);
             currentAudio.play().catch(error => {
                 console.log("Lỗi phát nhạc:", error);
-                alert("⚠️ Điện thoại chặn tự phát. Hãy chạm vào màn hình 1 lần rồi thử lại!");
             });
         }
 

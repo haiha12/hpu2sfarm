22d9b9c940117f00f356aee22126b3c1d630121c
haiha12 <xuanha221@gmail.com>
2026-01-18 08:40:33 +0700

    app.py

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ CORS(app, resources={r"/*": {"origins": CORS(app, resources={r"/*": {"origins": 
     "https://hpu2sfarm-6g8w.onrender.com", 
     "http://127.0.0.1:5500", 
     "http://localhost:5500"
-]}})}
-}
+]}})}}
+)
 model = None
 try:
     print("🔄 Đang tải model YOLOv11 Nano...")
@@ -137,6 +137,7 @@ def detect():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
 
 
 
